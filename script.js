8ccd9f87f234626392e3462337b44ebb993b8e65
Fedor <badgerf43@gmail.com>
2020-07-11 02:46:18 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+
+
+
+/*let persone = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(persone["name"]);
+//console.log(persone.name);
+
+*/
+
+/*let arr = ["plum.png", "orange.jpg", "apple.bmp"];
+
+console.log(arr[0]);
+
+*/
+
+
+//alert("hello world");
+
+
+
+/*let answer = confirm("r u sure?");
+
+//console.log(answer);
+
+
+
+//let answer = prompt("Есть ли вам 18?", "Yup");
+
+//console.log(typeof (arr));*/
+
+
+let incr = 10,
+    decr = 10;
+
+//incr++;
+//decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
